883eaf85d7a36b158c7db31c62291c8833be06cf
linjunxiong <Lin@Linjx-PC>
2016-11-13 17:37:16 +0800

    添加消息处理

---
+++ wxdiethealth/src/com/common/util/MessageUtil.java
@@ -0,0 +1,166 @@
+package com.common.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import javax.jms.TextMessage;
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+
+/**
+ * 解析微信发来的请求（XML）
+ * 返回string转成的xml
+ * @author admin
+ *
+ */
+public class MessageUtil {
+	
+	private static final String messageURL = "https://api.weixin.qq.com/cgi-bin/get_current_autoreply_info?access_token=";
+	
+	public static void main(String[] args) {
+		getAutoReplyInfo();
+	}
+	public static String getAutoReplyInfo() {
+		
+		String access_token = CommonUtil.getToken().getAccessToken();
+		String action = messageURL + access_token;
+		try {
+			URL url = new URL(action);
+			HttpURLConnection http = (HttpURLConnection) url.openConnection();
+
+			http.setRequestMethod("POST");
+			http.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+			http.setDoOutput(true);
+			http.setDoInput(true);
+			System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时30秒
+			System.setProperty("sun.net.client.defaultReadTimeout", "30000"); // 读取超时30秒
+
+			http.connect();
+			OutputStream os = http.getOutputStream();
+			os.write("".getBytes());// 传入参数
+			os.flush();
+			os.close();
+
+			InputStream is = http.getInputStream();
+			int size = is.available();
+			byte[] jsonBytes = new byte[size];
+			is.read(jsonBytes);
+			String message = new String(jsonBytes, "UTF-8");
+			System.out.println(message);
+			return "返回信息" + message;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "createMenu 失败";
+	}
+	
+	
+	/**
+     * 解析微信发来的请求（XML）
+     * @param request
+     * @return
+     */
+    public static Map<String, String> parseXml(HttpServletRequest request)
+            throws Exception {
+        // 将解析结果存储在HashMap中
+        Map<String, String> map = new HashMap<String, String>();
+
+        // 从request中取得输入流
+        InputStream inputStream = request.getInputStream();
+        // 读取输入流
+        SAXReader reader = new SAXReader();
+        Document document = (Document) reader.read(inputStream);
+        // 得到xml根元素
+        Element root = document.getRootElement();
+        // 得到根元素的所有子节点
+        @SuppressWarnings("unchecked")
+        List<Element> elementList = root.elements();
+
+        // 遍历所有子节点
+        for (Element e : elementList)
+            map.put(e.getName(), e.getText());
+
+        // 释放资源
+        inputStream.close();
+        inputStream = null;
+
+        return map;
+    }
+
+    /**
+     * 文本消息对象转换成xml
+     * @param textMessage 文本消息对象
+     * @return xml
+     */
+    public static String textMessageToXml(TextMessage textMessage) {
+        xstream.alias("xml", textMessage.getClass());
+        return xstream.toXML(textMessage);
+    }
+
+    /* *//** 
+     * 音乐消息对象转换成xml 
+     *  
+     * @param musicMessage 音乐消息对象 
+     * @return xml 
+     *//*  
+    public static String musicMessageToXml(MusicMessage musicMessage) {  
+        xstream.alias("xml", musicMessage.getClass());  
+        return xstream.toXML(musicMessage);  
+    }  */
+
+ /*   *//** 
+     * 图文消息对象转换成xml 
+     *  
+     * @param newsMessage 图文消息对象 
+     * @return xml 
+     *//*  
+    public static String newsMessageToXml(NewsMessage newsMessage) {  
+        xstream.alias("xml", newsMessage.getClass());  
+        xstream.alias("item", new Article().getClass());  
+        return xstream.toXML(newsMessage);  
+    } */ 
+    /** 
+     * 扩展xstream，使其支持CDATA块 
+     * xstream 还依赖xmlpull_1_0_5.jar许引入
+     */  
+    private static XStream xstream = new XStream(new XppDriver() {  
+        public HierarchicalStreamWriter createWriter(Writer out) {  
+            return new PrettyPrintWriter(out) {  
+                // 对所有xml节点的转换都增加CDATA标记  
+                boolean cdata = true;  
+                protected void writeText(QuickWriter writer, String text) {  
+                    if (cdata) {  
+                        writer.write("<![CDATA[");  
+                        writer.write(text);  
+                        writer.write("]]>");  
+                    } else {  
+                        writer.write(text);  
+                    }  
+                }  
+            };  
+        }  
+    }); 
+    
+    
+}
---
+++ wxdiethealth/src/com/common/util/TextMessage.java
@@ -0,0 +1,21 @@
+package com.common.util;
+
+public class TextMessage extends BaseMessage {
+    /**
+     * 回复的消息内容
+     */
+    private String Content;
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString()+"[Content:"+this.Content+"]";
+    }
+}
